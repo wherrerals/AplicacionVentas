75d4a660b39b85c60b1e45539c2c06fedc3ea493
TheChrow <wherrera.282@gmail.com>
2024-02-04 18:19:16 -0300

    Prueba de version practicas

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -7,8 +7,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import requests
-from django.http import JsonResponse
 
 @login_required
 def home(request):
