3f91bd569654f46f1f36ff7f9178548b323d1ec4
NicolasRuizMunoz <nruiz1@miuandes.cl>
2024-02-05 10:59:21 -0300

    test ignore

--- AplicacionVentas/settings.py
+++ AplicacionVentas/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'AplicacionVentas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/William Herrera/Documents/Proyectoledstudio/AplicacionVentas/gestionPedidos/templates/gestionPedidos/'],
+        'DIRS': ['/Users/nicor/Universidad/Practica/AplicacionVentas/gestionPedidos/templates/gestionPedidos/'],
         
         'APP_DIRS': True,
         'OPTIONS': {
