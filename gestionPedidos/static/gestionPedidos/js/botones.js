f8a7a9056371325a460aa7d51a877e342db07437
wherrerals <wherrera@ledstudio.cl>
2024-02-08 16:48:59 -0300

    trabajando en cotizaciones

--- gestionPedidos/static/gestionPedidos/js/botones.js
+++ gestionPedidos/static/gestionPedidos/js/botones.js
@@ -6,19 +6,6 @@ function cargarProductosGuardados() {
     }
 }
 
-function obtenerDatosProducto(productoId) {
-    $.ajax({
-        url: '/obtener-datos-producto/' + productoId + '/',
-        type: 'GET',
-        success: function(data) {
-            agregarProducto(data.productoCodigo, data.stock, data.precioActual, data.precioAnterior, data.maxDescuento);
-        },
-        error: function(xhr, status, error) {
-            console.error('Error al obtener los datos del producto:', error);
-        }
-    });
-}
-
 
 function agregarProducto(productoCodigo, stock, precioActual, precioAnterior, maxDescuento) {
     // Crear una nueva fila
@@ -26,6 +13,7 @@ function agregarProducto(productoCodigo, stock, precioActual, precioAnterior, ma
 
     // Definir el contenido de la fila
     newRow.innerHTML = `
+
     <td>
     <div class="row">
         <div class="col-md-11 col-xxl-6">
@@ -42,8 +30,8 @@ function agregarProducto(productoCodigo, stock, precioActual, precioAnterior, ma
         <div class="col-sm-12 col-lg-12 col-xl-11 col-xxl-10">
             <select class="form-select" style="font-size: 11px;">
                 <optgroup label="Bodega">
-                    <option value="12" selected>GR</option>
-                    <option value="13"></option>
+                    <option value="12">GR</option>
+                    <option value="13">LC</option>
                     <option value="14">PH</option>
                     <option value="15">ME</option>
                 </optgroup>
@@ -79,11 +67,30 @@ function agregarProducto(productoCodigo, stock, precioActual, precioAnterior, ma
 <td>
     <input class="form-control" type="text" style="width: 65px;">
 </td>
-<td class="font-weight-bold font-size-11 text-center">${precioActual}</td>
+<td class="font-weight-bold font-size-11 text-center">${precioActual}
+
+<td>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="eliminar-producto"
+        class="bi bi-trash" style="width: 18px;height: 18px;">
+        <path
+            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z">
+        </path>
+        <path
+            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z">
+        </path>
+    </svg>
+</td>
+
     `;
     
     // Agregar la fila a la tabla
     document.getElementById('productos').appendChild(newRow);
+
+    //Eliminar fila de la tabla
+    newRow.querySelector('.eliminar-producto').addEventListener('click', function() {
+        newRow.remove();
+    });
 
   };
 
@@ -105,3 +112,4 @@ function fechaHora(){
 
 }
 
+agregarProducto('N10100256', '12', '1.964.900', '2.999.900', '15%');
