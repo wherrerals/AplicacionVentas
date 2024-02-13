d88db1ecc1c3d989c4dd19183a4d0ebf1b8c1149
wherrerals <wherrera@ledstudio.cl>
2024-02-13 16:49:41 -0300

    Trabajando en template cotizacion

--- gestionPedidos/static/gestionPedidos/js/botones.js
+++ gestionPedidos/static/gestionPedidos/js/botones.js
@@ -9,88 +9,100 @@ function cargarProductosGuardados() {
 
 function agregarProducto(productoCodigo, stock, precioActual, precioAnterior, maxDescuento) {
     // Crear una nueva fila
-    var newRow = document.createElement('tr');
+    var newRow = document.createElement('tbody');
 
     // Definir el contenido de la fila
     newRow.innerHTML = `
-
-    <td>
-    <div class="row">
-        <div class="col-md-11 col-xxl-6">
-            <small>1)</small>&nbsp;&nbsp;
-            <small class="font-weight-bold">${productoCodigo}</small>
+<tr>
+    <td style="font-size: 12px;background: transparent;border-style: none;padding-bottom: 0px; " rowspan="2">
+        <div class="row">
+            <div class="col-md-11 col-xxl-6 style=" font-size: 14px;font-weight: bold;">
+                <small>1)</small>
+                <small>&nbsp;&nbsp;</small>
+                <small style="font-weight-bold">N10100256</small>
+            </div>
+            <div class="col-md-11 col-xxl-7 style="text-align: center;">
+                <img src="https://ledstudiocl.vtexassets.com/arquivos/ids/166187-80-auto/label-0.jpg" width="50" height="50" style="width: 50px;height: 50px;">
+            </div>
         </div>
-        <div class="col-md-11 col-xxl-7 text-center">
-            <img src="https://ledstudiocl.vtexassets.com/arquivos/ids/166187-80-auto/label-0.jpg" width="50" height="50">
+    </td>
+    <td style="font-size: 12px;background: transparent;border-style: none;padding-bottom: 0px; rowspan="2">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12 col-xl-11 col-xxl-10">
+              <select class="form-select" style="font-size: 11px;">
+                  <optgroup label="Bodega">
+                      <option value="12" selected="">GR</option>
+                      <option value="13">LC</option>
+                      <option value="14">PH</option>
+                      <option value="15">ME</option>
+                  </optgroup>
+              </select>
+            </div>
+            <div class="col" style="text-align: center;>
+                <small style="font-size: 12px;">Stock: ${stock}</small>
+            </div>
         </div>
-    </div>
-</td>
-<td>
-    <div class="row">
-        <div class="col-sm-12 col-lg-12 col-xl-11 col-xxl-10">
-            <select class="form-select" style="font-size: 11px;">
-                <optgroup label="Bodega">
-                    <option value="12">GR</option>
-                    <option value="13">LC</option>
-                    <option value="14">PH</option>
-                    <option value="15">ME</option>
-                </optgroup>
-            </select>
+    </td>
+    <td style="background: transparent;border-style: none;padding-bottom: 0px;" rowspan="2">
+        <div style="font-size: 12px;">
+            <small>${precioActual}</small>
         </div>
-        <div class="col text-center">
-            <small class="font-size-12">Stock: ${stock}</small>
+        <div style="font-size: 11px;">
+            <small style="color: rgb(153,153,153);">Antes: ${precioAnterior}</small>
         </div>
-    </div>
-</td>
-<td>
-    <div class="font-size-12">
-        <small>${precioActual}</small>
-    </div>
-    <div class="font-size-11">
-        <small class="text-muted">Antes: ${precioAnterior}</small>
-    </div>
-    <div class="row font-size-11">
-        <div class="col-sm-4 col-md-3 col-xl-2" style="padding-right: 0;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" style="font-size: 18px;">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
-            </svg>
+        <div class="row" style="font-size: 11px;">
+            <div class="col-sm-4 col-md-3 col-xl-2" style="padding-right: 0px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" style="font-size: 18px;">
+                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
+                </svg>
+            </div>
+            <div class="col-sm-7 col-md-8">
+                <small style="color: rgb(255,0,0);">Max: ${maxDescuento}</small>
+            </div>
         </div>
-        <div class="col-sm-7 col-md-8">
-            <small class="text-danger">Max: ${maxDescuento}</small>
+    </td>
+    <td style="font-size: 12px;background: transparent;border-style: none;">
+        <div>
+            <input class="form-control" type="text" style="font-size: 12px;width: 40px;">
         </div>
-    </div>
-</td>
-<td>
-    <input class="form-control" type="text">
-</td>
-<td class="font-size-11 text-center">${precioActual}</td>
-<td>
-    <input class="form-control" type="text" style="width: 65px;">
-</td>
-<td class="font-weight-bold font-size-11 text-center">${precioActual}
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="eliminar-producto"
-class="bi bi-trash" style="width: 18px;height: 18px;">
-<path
-    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z">
-</path>
-<path
-    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z">
-</path>
-</svg>
-
-
+    </td>
+    <td style="font-size: 11px;background: transparent;font-weight: bold;border-style: none;text-align: center;">${precioActual}</td>
+    <td style="font-size: 12px;background: transparent;border-style: none;">
+        <input class="form-control" type="text" style="width: 65px;">
+    </td>
+    <td style="font-size: 11px;background: transparent;font-weight: bold;border-style: none;text-align: center;">
+        <span>${precioActual}</span>
+    </td>
+</tr>
+    <tr style="font-size: 12px;background: transparent;">
+        <td style="font-size: 11px;background: transparent;padding-top: 0px;border-style: none;padding-bottom: 0px;"colspan="3">
+            <input class="form-control" type="text" placeholder="Comentario" style="font-size: 12px;">
+        </td>
+        <td style="background: transparent;padding-top: 8px;padding-left: 50px;border-style: none;padding-bottom: 0px;">
+            <a class="navbar-brand d-flex align-items-center" href="#" style="width: 18px;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" id="eliminar-producto" class="bi bi-trash" style="width: 18px;height: 18px;">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z">
+            </path>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z">
+            </path>
+        </svg>
+    </tr>
+    <tr style="font-size: 12px;background: transparent;">
+        <td colspan="7" style="padding-top: 0px;background: transparent;"><span>APLIQUÉ MURO PHILIPS SPOT STARSIMPLE 4.5W LUZ CÁLIDA GRIS</span></td>
+    </tr>
     `;
     
     // Agregar la fila a la tabla
     document.getElementById('productos').appendChild(newRow);
 
     //Eliminar fila de la tabla
-    newRow.querySelector('.eliminar-producto').addEventListener('click', function() {
+    newRow.querySelector('#eliminar-producto').addEventListener('click', function() {
         newRow.remove();
     });
 
 };
 
+/*
 // Función para guardar los productos en localStorage
 function guardarProductos() {
     localStorage.setItem('productos', document.getElementById('productos').innerHTML);
@@ -103,6 +115,7 @@ function cargarProductosGuardados() {
     }
 }
 
+*/
 window.addEventListener('DOMContentLoaded', function() {
     cargarProductosGuardados();
 
@@ -112,6 +125,7 @@ window.addEventListener('DOMContentLoaded', function() {
         botonAgregarProductos.addEventListener('click', agregarProducto);
     }
 });
+
 
 function fechaHora(){
 
