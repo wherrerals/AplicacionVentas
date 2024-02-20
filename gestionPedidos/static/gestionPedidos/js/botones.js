d795dfdc9c096fc31d381e47248afcecaef71007
NicolasRuizMunoz <nruiz1@miuandes.cl>
2024-02-19 21:21:51 -0300

    Boton eliminar

--- gestionPedidos/static/gestionPedidos/js/botones.js
+++ gestionPedidos/static/gestionPedidos/js/botones.js
@@ -143,7 +143,7 @@ function agergardireccion(){
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"> </path>
                 </svg>
             </a>
-        </div>
+        </div> 
         
         
         <div class="col-sm-1" style="padding-right: 0px;padding-left: 10px;">
@@ -285,8 +285,8 @@ function agregarontacto(){
         </div>
 
         <div class="col-sm-1" style="padding-right: 0px;padding-left: 10px;">
-            <a class="navbar-brand d-flex align-items-center" href="lista_cotizaciones.html" style="width: 32px;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-backspace" style="font-size: 24px;">
+            <a class="navbar-brand d-flex align-items-center"  style="width: 32px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-backspace" style="font-size: 24px;" id="eliminar_contacto">
                     <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"> </path>
                     <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"> </path>
                 </svg>
@@ -341,6 +341,10 @@ function agregarontacto(){
 
 
     document.getElementById('tab-2').appendChild(newRow);
+
+    newRow.querySelector('#eliminar_contacto').addEventListener('click', function() {
+        newRow.remove();
+    });
 
 }
 
