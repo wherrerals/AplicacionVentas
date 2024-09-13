8d7f28c23623e5cac58467857df511171392e9e9
NicolasRuizMunoz <nruiz1@miuandes.cl>
2024-09-13 12:32:13 -0300

    clientes html 90%, falta ver porque se desajusta el rut al volver a persona

--- showromVentasApp/static/gestionPedidos/js/interactividadUsuario.js
+++ showromVentasApp/static/gestionPedidos/js/interactividadUsuario.js
@@ -33,4 +33,24 @@ function toggleRepeatPassword() {
 
   toggleRepeatPassword();
 
+// archivo formularioReactivo.js
+
+function cambiarLabel(razonSocialRadioName, nombreLabelId, apellidoInputId, apellidolabelId) {
+    var razonSocialRadio = document.getElementsByName(razonSocialRadioName);
+    var nombreLabel = document.getElementById(nombreLabelId);
+    var apellidoInput = document.getElementById(apellidoInputId);
+    var apellidolabel = document.getElementById(apellidolabelId);
+
+    if (razonSocialRadio[0].checked) {
+        nombreLabel.textContent = 'Nombre';
+        apellidoInput.style.display = 'block'; // Mostrar input de apellido
+        apellidolabel.style.display = 'block'; // Mostrar label de apellido
+    } else if (razonSocialRadio[1].checked) {
+        nombreLabel.textContent = 'R. Social';
+        apellidoInput.style.display = 'none'; // Ocultar input de apellido
+        apellidolabel.style.display = 'none'; // Ocultar label de apellido
+    }
+}
+
+
 
