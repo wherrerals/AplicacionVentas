73609f46dab400a7788021803ff3d4e2975b2559
NicolasRuizMunoz <nruiz1@miuandes.cl>
2024-11-27 18:09:07 -0300

    Limpiar buscador prod y formato del valido hasta

--- showromVentasApp/static/gestionPedidos/js/ajax_solicitudes.js
+++ showromVentasApp/static/gestionPedidos/js/ajax_solicitudes.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $('#inputNumero').on('input', function(){ //escucha el evento de entrada en input con #inputNumero
+$(document).ready(function () {
+    $('#inputNumero').on('input', function () { // escucha el evento de entrada en input con #inputNumero
         let numero = $(this).val();
-        if(numero){
+        if (numero) {
             let buscarProductosUrl = '/ventas/buscarproductos/'; // URL para buscar productos
             $.ajax({ // realiza una solicitud ajax al servidor a la url buscar/
-                url: buscarProductosUrl, 
+                url: buscarProductosUrl,
                 data: {
-                    'numero': numero //envia los datos ingresados por el usuario al servidor (sku)
+                    'numero': numero // envia los datos ingresados por el usuario al servidor (sku)
                 },
-                dataType: 'json',  //indica el formato en el que espera los datos
-                success: function(data){ // si la respuesta del servidor es exitosa se ejecuta la funcion
-                    $('#resultados').empty(); //limpia resultados anteriores
-                    if(data.resultados && data.resultados.length > 0){ //comprueba si hay resultados y si estos son mayores que 0
-                        data.resultados.forEach(function(resultado){ // si se obtienen resultados itera sonbre estos para realizar:
+                dataType: 'json', // indica el formato en el que espera los datos
+                success: function (data) { // si la respuesta del servidor es exitosa se ejecuta la funcion
+                    $('#resultados').empty(); // limpia resultados anteriores
+                    if (data.resultados && data.resultados.length > 0) { // comprueba si hay resultados y si estos son mayores que 0
+                        data.resultados.forEach(function (resultado) { // si se obtienen resultados itera sobre estos para realizar:
                             // Convertir valores a números si son cadenas
                             resultado.precio = parseFloat(resultado.precio);
                             resultado.precioAnterior = resultado.precioAnterior;
                             resultado.maxDescuento = parseFloat(resultado.maxDescuento);
                             resultado.stockTotal = parseInt(resultado.stockTotal);
-                            
+
                             // Agregar elementos al resultado
                             var productoElemento = document.createElement('p');
                             productoElemento.className = 'agregar_productos';
@@ -32,7 +32,7 @@ $(document).ready(function(){
                             productoElemento.textContent = `${resultado.codigo} | ${resultado.nombre} | Stock Total: ${resultado.stockTotal}`;
 
                             // Agregar evento de clic al elemento
-                            productoElemento.addEventListener('click', function() {
+                            productoElemento.addEventListener('click', function () {
                                 let codigo = this.getAttribute('data-codigo');
                                 let nombre = this.getAttribute('data-nombre');
                                 let imagen = this.getAttribute('data-imagen');
@@ -40,25 +40,27 @@ $(document).ready(function(){
                                 let stockTotal = parseInt(this.getAttribute('data-stockTotal'));
                                 let precioLista = parseFloat(this.getAttribute('data-precioAnterior'));
                                 let precioDescuento = parseFloat(this.getAttribute('data-maxDescuento'));
-                                
-                                agregarProducto(codigo, nombre, imagen, precioVenta, stockTotal, precioLista, precioDescuento, cantidad, sucursal); //Ejecuta la funcion agregar producto
 
+                                agregarProducto(codigo, nombre, imagen, precioVenta, stockTotal, precioLista, precioDescuento); // Ejecuta la función agregar producto
+
+                                // Limpia el input #inputNumero al seleccionar un producto
+                                $('#inputNumero').val('');
                             });
 
-                            $('#resultados').append(productoElemento); //Agrega los resultados en el contenedor
-                            
-                            $('#resultados').on('click', '*', function() {
+                            $('#resultados').append(productoElemento); // Agrega los resultados en el contenedor
+
+                            $('#resultados').on('click', '*', function () {
                                 // Limpiar el div #resultados al hacer clic en cualquier elemento dentro de él
                                 $('#resultados').empty();
                             });
                         });
                     } else {
-                        $('#resultados').text('No se encontraron resultados'); //Si no encuentra resultados muestra el mensaje
+                        $('#resultados').text('No se encontraron resultados'); // Si no encuentra resultados muestra el mensaje
                     }
                 }
             });
         } else {
-            $('#resultados').empty(); //Si el usuario elimina el codito borra todos los resultados.
+            $('#resultados').empty(); // Si el usuario elimina el número borra todos los resultados.
         }
     });
 });
