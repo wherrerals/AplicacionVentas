2a15779bfb446a8757ffe624423498cc0d13839e
wherrerals <wherrera@ledstudio.cl>
2025-01-24 19:01:19 -0300

    actualizando masters 2

--- master.sql
+++ master.sql
@@ -3,3 +3,10 @@
 use led_studio;
 INSERT INTO ConfiEmpresa (razonsocial, rut, direccion, rentabilidadBrutaMin, rentabilidadBrutaConAut)
 VALUES ('Studio Group SpA', '76927160-0', 'Avenida Las Condes 7363, Las Condes', 50, 40);
+
+INSERT INTO confiDescuento (codigo, descripcion, tipoVenta, limiteDescuentoMaximo, tipoDeMarca)
+VALUES 
+('1', 'TIENDA - Propios', 'Tienda', 15, 'Propios'),
+('2', 'PROYECTO - Propios', 'Proyecto', 25, 'Propios'),
+('3', 'TIENDA - TERCEROS', 'Tienda', 10, 'Terceros'),
+('4', 'PROYECTOS - TERCEROS', 'Proyecto', 15, 'Terceros');
