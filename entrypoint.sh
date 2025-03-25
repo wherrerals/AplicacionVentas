70ae8a02c8d86e43b7ac6b41c2be281d415be94a
wherrerals <wherrera@ledstudio.cl>
2025-03-25 16:11:18 -0300

    fix_hot(entrypoint.sh): Solucionar novedad de la cncanitdad de procesos que puede manejar la app

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ python manage.py migrate --settings=config.settings.production
 
 
 #gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:7000 --timeout 180
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:7000 --workers=17 --threads=2 --timeout 180
+#gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:7000 --workers=17 --threads=4 --timeout 300 --log-level debug
+gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application \
+--bind 0.0.0.0:7000 --workers=16 --threads=4 --timeout=300 \
+--keep-alive 5 --max-requests 1000 --max-requests-jitter 100 --log-level debug
 
