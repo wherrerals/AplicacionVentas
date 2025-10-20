4cca1b623e2ff1ad39084874759e1341640d5514
wherrerals <wherrera@ledstudio.cl>
2025-10-20 16:50:23 -0300

    update: productos.js, Actualizar estado de stock en 0 - Agregar icono bootstrap - Agregar condicional de si es 0 mostrar dinamico

--- presentation/static/gestionPedidos/js/productos.js
+++ presentation/static/gestionPedidos/js/productos.js
@@ -57,6 +57,18 @@ class Producto {
             // Mostrar el stock total
             const stockTotalElem = row.querySelector('[name="stock_total"]');
             stockTotalElem.textContent = `Total: ${stockTotal}`;
+            stockTotalElem.style.color = stockTotal === 0 ? 'red' : 'black';
+
+            let warningIcon = row.querySelector('#warning_container');
+            console.log("capturado el warning icon:", warningIcon);
+
+            if (stockTotal === 0) {
+                console.log("No hay stock disponible para el producto:", this.productoCodigo);
+                warningIcon.hidden = false;
+            } else {
+                console.log("Hay stock disponible para el producto:", this.productoCodigo);
+                warningIcon.hidden = true;
+            }
 
             // Obtener el value de la bodega seleccionada
             const selectBodega = row.querySelector('.form-select');
@@ -115,6 +127,12 @@ class Producto {
                         <div class="col" style="text-align: center;">
                             <small style="font-size: 12px;" name="stock_bodega">Stock:  </small>
                             <small style="font-size: 12px;" id="stock_total" name="stock_total">Total: </small>
+                            <div id="warning_container" style="display: inline-block;" hidden>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16" style="color: rgba(255, 7, 7, 1);margin-left: 5px;" name="warning_icon">
+                                <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+                                <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                            </svg>
+                            </div>
                         </div>
                     </div>
                 </td>
