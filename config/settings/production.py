0c47138f3c4d482f34e24372431e21549f6fa2f7
wherrera@ledstudio.cl <wherrera.282@gmail.com>
2025-12-23 22:04:06 -0300

    deploy to production

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["ventas.ledstudio.cl"] 
+ALLOWED_HOSTS = ["ventas.ledstudio.cl", "studio-go-dev"]
 
 # Base service Layer
 API_BASE_URL = os.environ.get("API_BASE_URL")
